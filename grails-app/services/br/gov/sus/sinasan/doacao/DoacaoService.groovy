ce2fea71b0804c5fd11ef99e64c44d0187010e31
Marcelo Assis <marceleira@gmail.com>
2016-09-11 20:08:07 -0300

    Service para processar arquivos do mainframe

---
+++ grails-app/services/br/gov/sus/sinasan/doacao/DoacaoService.groovy
@@ -0,0 +1,63 @@
+package br.gov.sus.sinasan.doacao
+
+import br.gov.sus.sinasan.doacao.exceptions.BusinessRuleException
+import grails.transaction.Transactional
+
+@Transactional
+class DoacaoService {
+
+    def processaFileDoacoes(Integer codigoLaboratorio, File arquivo) throws BusinessRuleException {
+
+        UnidadeLaboratorial unidadeLaboratorial = UnidadeLaboratorial.findByCodigo(codigoLaboratorio)
+
+        if(!unidadeLaboratorial) {
+            throw new BusinessRuleException("Unidade Laboratorial de código '$codigoLaboratorio' não cadastrada no sistema")
+        }
+
+        BufferedReader br = arquivo.newInputStream().newReader()
+
+        String linha
+        def dados = [:]
+        for(def i = 0; linha = br.readLine() ; i++) {
+
+            if(i == 0) {
+                linha = linha.substring(0, 375)
+            } else {
+                linha = linha.substring(0, 374)
+                linha = " "+linha;
+            }
+
+            dados.doador = [:]
+            dados.doador.nomeCompleto       = linha.substring(0, 64).trim()
+            dados.doador.sexo               = Sexo.findByCodigo(linha.substring(65, 66).trim().toUpperCase())
+            dados.doador.cpf                = linha.substring(66, 77).trim()
+            dados.doador.dataNascimento     = linha.substring(77, 85).trim()
+            dados.doador.endereco           = linha.substring(85, 184).trim()
+            dados.doador.telefone           = linha.substring(184, 200).trim()
+            dados.doador.email              = linha.substring(200, 264).trim()
+            dados.doador.naturalidade       = linha.substring(264, 296).trim()
+            dados.doador.nacionalidade      = linha.substring(296, 328).trim()
+
+            dados.doacao = [:]
+            dados.doacao.dataHoraAgendamento = linha.substring(328, 336) + linha.substring(336, 340)
+            dados.doacao.unidadeLaboratorial = unidadeLaboratorial
+
+            dados.bolsa = [:]
+            dados.bolsa.codigo             = linha.substring(340, 372).trim()
+            dados.bolsa.grupoSanguineo     = GrupoSanguineo.findByCodigo(linha.substring(372, 375).trim())
+
+            Doador doador = Doador.findByCpf(dados.doador.cpf) ?: new Doador()
+            doador.properties = dados.doador
+            doador.save(flush: true, failOnError: true);
+
+            Doacao doacao = new Doacao(dados.doacao)
+            doacao.doador = doador
+            doacao.save(flush: true, failOnError: true)
+
+            BolsaSangue bolsaSangue = new BolsaSangue(dados.bolsa)
+            bolsaSangue.doacao = doacao
+            bolsaSangue.save(flush: true, failOnError: true)
+        }
+
+    }
+}
