2f328217f6f19f69533c8c30c0de184c1b631291
Marcelo Assis <marceleira@gmail.com>
2016-10-04 15:22:33 -0300

    Webservice para receber remessa de doação

--- grails-app/controllers/br/gov/sus/sinasan/doacao/WsController.groovy
+++ grails-app/controllers/br/gov/sus/sinasan/doacao/WsController.groovy
@@ -1,14 +1,19 @@
 package br.gov.sus.sinasan.doacao
 
+import br.gov.sus.sinasan.doacao.exceptions.BusinessRuleException
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['ROLE_WEBSERVICE','ROLE_ADMIN'])
 class WsController {
 
+    def doacaoService
+    def messageSource
+
     def remessaDoacoes = {
     }
 
+    // obter dados da bolsa de sangue
     def rastrearBolsa = {
         String codigoBolsa = params?.id
         def dados = [:]
@@ -40,4 +45,54 @@ class WsController {
 
         render dados as JSON
     }
+
+    def remessaDoacao = {
+
+        params.dataAgendamento = params?.dataAgendamento ? params.dataAgendamento : ''
+        params.horaAgendamento = params?.horaAgendamento ? params.horaAgendamento : ''
+
+        UnidadeLaboratorial unidadeLaboratorial = UnidadeLaboratorial.get(Long.parseLong(params?.id as String))
+
+        def dados = [:]
+
+        dados.doador = [:]
+        dados.doador.nomeCompleto       = params?.nomeDoador?.trim()
+        dados.doador.sexo               = Sexo.findByCodigo(params?.codSexo as String)
+        dados.doador.cpf                = params?.cpfDoador
+        dados.doador.dataNascimento     = params?.dataNascimentoDoador
+        dados.doador.endereco           = params?.enderecoDoador
+        dados.doador.telefone           = params?.telefoneDoador
+        dados.doador.email              = params?.emailDoador
+        dados.doador.naturalidade       = params?.naturalidadeDoador
+        dados.doador.nacionalidade      = params?.nacionalidadeDoador
+
+        dados.doacao = [:]
+        dados.doacao.dataHoraAgendamento = params?.dataAgendamento + params?.horaAgendamento
+        dados.doacao.unidadeLaboratorial = unidadeLaboratorial
+
+        dados.bolsa = [:]
+        dados.bolsa.codigo             = params?.codigoBolsa
+        dados.bolsa.grupoSanguineo     = GrupoSanguineo.findByCodigo(params?.codGrupoSanguineo as String)
+
+        try {
+            doacaoService.salvarDoacao(dados.doador as Doador, dados.doacao as Doacao, dados.bolsa as BolsaSangue)
+        } catch (BusinessRuleException e) {
+
+            def locale = Locale.default
+            def listaErros = [:].withDefault { [] }
+            for (fieldErrors in e.objeto.errors) {
+                for (error in fieldErrors.allErrors) {
+                    listaErros[error.field] << messageSource.getMessage(error, locale)
+                }
+            }
+
+            def msg = [erro: 1, msg_erro: listaErros]
+            render msg as JSON
+            return
+        }
+
+        def msg = [erro: 0, msg_erro: "Remessa processada com sucesso!"]
+        render msg as JSON
+    }
+
 }
