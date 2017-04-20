76062e5e8cea913e65bb924b3bd1f785b480bd16
Marcelo Assis <marceleira@gmail.com>
2017-04-20 15:42:04 -0300

    Problema de timeout no jdbc

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -20,7 +20,7 @@ environments {
             dbCreate = "update"
 //            dbCreate = "none"
 //            dbCreate = "create-drop"
-            url = "jdbc:mysql://localhost:3306/sinasan"
+            url = "jdbc:mysql://localhost:3306/sinasan?autoReconnect=true"
         }
     }
     test {
@@ -28,7 +28,7 @@ environments {
             username = "sinasan"
             password = "app"
             dbCreate = "update"
-            url = "jdbc:mysql://localhost:3306/sinasan"
+            url = "jdbc:mysql://localhost:3306/sinasan?autoReconnect=true"
         }
     }
     production {
@@ -36,7 +36,7 @@ environments {
             username = "sinasan"
             password = "app"
             dbCreate = "update"
-            url = "jdbc:mysql://localhost:3306/sinasan"
+            url = "jdbc:mysql://localhost:3306/sinasan?autoReconnect=true"
         }
     }
 }
